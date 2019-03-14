440ecb9e581721c72529bf63678476e9bf5917cd
venkat <penubothuvenkannachowdary@gmail.com>
2019-03-14 21:27:36 +0530

    Updates

---
+++ precache-manifest.0eb82abc1361e12ccec6990c1b0e2d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c619cde00cb29466aa4",
+    "url": "/subscriber/static/js/runtime~main.f71dec9c.js"
+  },
+  {
+    "revision": "8d64106fb412ca801217",
+    "url": "/subscriber/static/js/main.28fe1f7e.chunk.js"
+  },
+  {
+    "revision": "86e5a1a750e42c989905",
+    "url": "/subscriber/static/js/2.f920d325.chunk.js"
+  },
+  {
+    "revision": "8d64106fb412ca801217",
+    "url": "/subscriber/static/css/main.62c9a97d.chunk.css"
+  },
+  {
+    "revision": "0f66e10a1b82b8ea44abe65b3de56abf",
+    "url": "/subscriber/index.html"
+  }
+];
